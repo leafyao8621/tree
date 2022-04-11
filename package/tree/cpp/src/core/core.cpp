2f35c2d8d1f9ced8a0cf982376a9023ca22b7ba4
Yi Yao <leaf_yao8621@hotmail.com>
2022-04-11 13:54:04 -0700

    Started cpp sum up

--- package/tree/cpp/src/core/core.cpp
+++ package/tree/cpp/src/core/core.cpp
@@ -30,6 +30,7 @@ Core::Tree::Tree(std::string fn) {
     this->nodes[0]->down_calculated = true;
     this->nodes[0]->down_sum = this->nodes[0]->value;
     this->root = this->nodes[0];
+    this->up_calculated = false;
 }
 
 Core::Tree::~Tree() {
@@ -64,5 +65,34 @@ double Core::Tree::max_sum_down() {
             stack.push_back(cur->right);
         }
     }
+    return out;
+}
+
+double Core::Tree::max_sum_up() {
+    if (!this->root) {
+        throw Core::Tree::Err::EMPTY_TREE;
+    }
+    double out = this->root->down_sum;
+    std::list<Node*> stack;
+    stack.push_back(this->root);
+    // for (; stack.size();) {
+    //     Node *cur = stack.back();
+    //     stack.pop_back();
+    //     if (cur->down_sum > out) {
+    //         out = cur->down_sum;
+    //     }
+    //     if (cur->left) {
+    //         if (!cur->left->down_calculated) {
+    //             cur->left->down_sum = cur->down_sum + cur->left->value;
+    //         }
+    //         stack.push_back(cur->left);
+    //     }
+    //     if (cur->right) {
+    //         if (!cur->right->down_calculated) {
+    //             cur->right->down_sum = cur->down_sum + cur->right->value;
+    //         }
+    //         stack.push_back(cur->right);
+    //     }
+    // }
     return out;
 }
